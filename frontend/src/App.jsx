d331cdb51f1206e9a7be50e0bf91d0c000e5992d
vatsal81 <vatsalmarakana357159@gmail.com>
2026-05-01 13:36:46 +0530

    feat: re-activate About Us page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -138,8 +138,9 @@ function App() {
             <Route path="export" element={<Navigate to="../global-export" replace />} />
             <Route path="blog" element={<Blog />} />
             <Route path="iec-registration" element={<IECRegistration />} />
-            {/* <Route path="about" element={<About />} /> */}
+            <Route path="about" element={<About />} />
             <Route path="contact" element={<Contact />} />
+
 
             <Route path="login" element={<Login />} />
             <Route path="admin-portal" element={<ProtectedAdminRoute />} />
