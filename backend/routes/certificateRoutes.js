9bc45943e9eecca7672df3538ee1144713612b7d
vatsal81 <vatsalmarakana357159@gmail.com>
2026-05-02 13:29:20 +0530

    chore: implement proper monorepo CI/CD and unified dev scripts

--- backend/routes/certificateRoutes.js
+++ backend/routes/certificateRoutes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const certificateController = require('../controllers/certificateController');
 
-router.get('/', certificateController.getCertificates);
+router.get('/', (req, res, next) => {
+    console.log('GET /api/certificates - Request received');
+    next();
+}, certificateController.getCertificates);
 router.post('/', certificateController.createCertificate);
 router.put('/:id', certificateController.updateCertificate);
 router.delete('/:id', certificateController.deleteCertificate);
